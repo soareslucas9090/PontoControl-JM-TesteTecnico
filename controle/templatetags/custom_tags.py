e6c3d9a07e9af3e6cc0bc353b1c8f9f88b2bfb79
Lucas Soares <soareslukas9090@gmail.com>
2025-01-02 12:38:13 -0300

    doc: documentação de classe e atributos de custom_tags.py

--- controle/templatetags/custom_tags.py
+++ controle/templatetags/custom_tags.py
@@ -5,4 +5,14 @@ register = template.Library()
 
 @register.filter(name="add_class")
 def add_class(value, arg):
+    """
+    Adiciona uma classe CSS a um campo de formulário no Django.
+
+    Parâmetros:
+        value (BoundField): O campo do formulário a ser modificado.
+        arg (str): A classe CSS a ser adicionada ao widget.
+
+    Retorna:
+        SafeText: O widget do campo renderizado com a classe CSS adicionada.
+    """
     return value.as_widget(attrs={"class": arg})
