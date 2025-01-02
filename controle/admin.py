a0ee72b991c7c46505ab34e5d003471cfceec37f
Lucas Soares <soareslukas9090@gmail.com>
2025-01-02 12:36:27 -0300

    doc: documentação de classes e atributos de admin.py

--- controle/admin.py
+++ controle/admin.py
@@ -5,6 +5,18 @@ from .models import Usuario
 
 
 class UsuarioAdmin(UserAdmin):
+    """
+    Configuração de exibição e gerenciamento do modelo Usuario no Django Admin.
+
+    Atributos:
+        model (Model): Define o modelo associado a este admin.
+        list_display (tupla): Campos exibidos na lista de usuários.
+        fieldsets (tupla): Define as seções e campos ao visualizar/editar um usuário.
+        add_fieldsets (tupla): Define as seções e campos ao adicionar um novo usuário.
+        search_fields (tupla): Campos utilizados para busca.
+        ordering (tupla): Define a ordenação padrão na lista de usuários.
+    """
+
     model = Usuario
     list_display = ("cpf", "funcionario", "is_superuser")
     fieldsets = (
@@ -24,7 +36,7 @@ class UsuarioAdmin(UserAdmin):
             },
         ),
         (
-            "Funcinário",
+            "Funcionário",
             {
                 "classes": ("wide",),
                 "fields": ("funcionario",),
